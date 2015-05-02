31db831a939c8d3f6463eec1b044c5a719b6b034
Louis Beaudoin <louis@embedded-creations.com>
2015-05-02 16:38:26 -0400

    Revert "Add code from JumpToAppWithOffset"
    This reverts commit c8276a81cc2d5715a661cd1a3f32a5242bf05a28.

--- AnimatedGIFs.ino
+++ AnimatedGIFs.ino
@@ -62,67 +62,6 @@
 #include "GIFDecoder.h"
 
 #define DISPLAY_TIME_SECONDS 10
-// the assembly code must be run inside a C, not C++ function
-#ifdef __cplusplus
-extern "C" {
-#endif
-void jumpToApplicationAt0x38080() {
-  /* Load stack pointer and program counter from start of new program */
-  asm("movw r0, #0x8080");
-  asm("movt r0, #0x0003");
-  asm("ldr sp, [r0]");
-  asm("ldr pc, [r0, #4]");
-}
-#ifdef __cplusplus
-}
-#endif
-
-
-/*
- * These are the minimum peripherals that needed to be disabled to allow the
- * uTasker USB-MSD application to work.  You may need to reset more peripherals
- * depending on the application you are running, and what other peripherals
- * your sketch uses if you add more to this example than just blinking an LED
-*/
-void resetPeripherals() {
-  // set SPI back to normal
-  SPCR.disable_pins();
-  SPI0_MCR = SPI_MCR_HALT | SPI_MCR_MDIS;
-  SPI0_CTAR1 = 0;
-  SPI0_CTAR0 = 0;
-  SIM_SCGC6 &= ~(SIM_SCGC6_SPI0);
-
-
-  /* set (some of) USB back to normal */
-  NVIC_DISABLE_IRQ(IRQ_USBOTG);
-  NVIC_CLEAR_PENDING(IRQ_USBOTG);
-  SIM_SCGC4 &= ~(SIM_SCGC4_USBOTG);
-
-  /* disable all GPIO interrupts */
-  NVIC_DISABLE_IRQ(IRQ_PORTA);
-  NVIC_DISABLE_IRQ(IRQ_PORTB);
-  NVIC_DISABLE_IRQ(IRQ_PORTC);
-  NVIC_DISABLE_IRQ(IRQ_PORTD);
-  NVIC_DISABLE_IRQ(IRQ_PORTE);
-
-  /* set (some of) ADC1 back to normal */
-  // wait until calibration is complete
-  while(ADC1_SC3 & ADC_SC3_CAL);
-
-  // clear flag if calibration failed
-  if(ADC1_SC3 & 1<<6)
-    ADC1_SC3 |= 1<<6;
-
-  // clear conversion complete flag (which could trigger ISR otherwise)
-  if(ADC1_SC1A & 1<<7)
-    ADC1_SC1A |= 1<<7;
-
-  /* set some clocks back to default/reset settings */
-  MCG_C1 = MCG_C1_CLKS(2) | MCG_C1_FRDIV(4);
-  SIM_CLKDIV1 = 0;
-  SIM_CLKDIV2 = 0;
-}
-
 
 // range 0-255
 const int defaultBrightness = 255;
